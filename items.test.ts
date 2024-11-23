35e129d5a05e18eb34160be57b0634ee6ed65f16
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 00:10:05 +0900

    テストコードの可読性を向上させる

--- items.test.ts
+++ items.test.ts
@@ -2,10 +2,13 @@ import { assert } from "jsr:@std/assert";
 import { MaterialItem } from "./items.ts";
 
 Deno.test("MaterialItem生成", () => {
+  // Act
   const materialItem = MaterialItem.new("マジックグラス", [
     { name: "植物類" },
     { name: "薬の材料" },
   ]);
+
+  // Assert
   assert(materialItem.name === "マジックグラス");
   assert(materialItem.category[0].name === "植物類");
   assert(materialItem.category[1].name === "薬の材料");
