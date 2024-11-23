7cd0d89abd5de6408a94c81601b4afa5b22b9473
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 01:39:56 +0900

    トトリのアトリエのデータ置き場を作る

---
+++ dataset/totori.ts
@@ -0,0 +1,26 @@
+import { MaterialItem, SynthesisItem } from "../items.ts";
+
+export const materialItems: MaterialItem[] = [
+  MaterialItem.new("マジックグラス", [
+    { name: "植物類" },
+    { name: "薬の材料" },
+    { name: "中和剤" },
+  ]),
+  MaterialItem.new("マンドラゴラの根", [
+    { name: "薬の材料" },
+    { name: "エリキシル" },
+    { name: "毒の材料" },
+    { name: "中和剤" },
+  ]),
+  MaterialItem.new("水", [{ name: "液体" }, { name: "中和剤" }]),
+];
+
+export const synthesisItems: SynthesisItem[] = [
+  SynthesisItem.new("ヒーリングサルヴ", [{ name: "薬品" }], 1, {
+    items: [
+      { kind: "category", category: { name: "植物類" } },
+      { kind: "specific", item: { name: "マンドラゴラの根" } },
+      { kind: "specific", item: { name: "水" } },
+    ],
+  }),
+];
