229aa7d890d01c75949691ec0621e4ea52dc7fd4
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 20:13:25 +0900

    ライザ：素材アイテム追記

--- dataset/ryza.ts
+++ dataset/ryza.ts
@@ -4,6 +4,12 @@ export const materialItems: MaterialItem[] = [
   MaterialItem.new("きれいな水", [{ name: "水" }]),
   MaterialItem.new("ヤギミルク水", [{ name: "水" }, { name: "食材" }]),
   MaterialItem.new("ナナシ草", [{ name: "植物" }]),
+  MaterialItem.new("アオツメクサ", [{ name: "植物" }, { name: "花" }]),
+  MaterialItem.new("ヴァッサ麦", [{ name: "植物" }, { name: "食材" }]),
+  MaterialItem.new("スイートリーフ", [{ name: "植物" }, { name: "食材" }]),
+  MaterialItem.new("うに", [{ name: "うに" }]),
+  MaterialItem.new("病忘れの花", [{ name: "花" }, { name: "薬の材料" }]),
+  MaterialItem.new("北風の花", [{ name: "花" }, { name: "毒の材料" }]),
   MaterialItem.new("陽気な蜜花", [{ name: "花" }, { name: "調味料" }]),
 ];
 
