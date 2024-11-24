ab71a3523c1230538db4a5bed806dff4553ee1ba
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 14:56:50 +0900

    RED: 少し複雑な循環調合の経路

--- detect.test.ts
+++ detect.test.ts
@@ -10,6 +10,18 @@ Deno.test("循環調合を探す", () => {
       name: "マジックグラス",
       category: [{ name: "植物類" }, { name: "薬の材料" }, { name: "中和剤" }],
     },
+    {
+      name: "ラーメル麦",
+      category: [{ name: "食材" }, { name: "植物類" }],
+    },
+    {
+      name: "カーエン石",
+      category: [{ name: "鉱石" }, { name: "火薬" }],
+    },
+    {
+      name: "おいしい水",
+      category: [{ name: "水" }],
+    },
   ];
   const synthesisItems: SynthesisItem[] = [
     {
@@ -23,6 +35,41 @@ Deno.test("循環調合を探す", () => {
         ],
       },
     },
+    {
+      name: "中和剤・赤",
+      category: [{ name: "中和剤" }],
+      synthesisLevel: 2,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "火薬" } },
+          { kind: "category", category: { name: "水" } },
+        ],
+      },
+    },
+    {
+      name: "ラーメル麦粉",
+      category: [{ name: "食材" }, { name: "火薬" }],
+      synthesisLevel: 3,
+      recipe: {
+        items: [
+          { kind: "specific", item: { name: "ラーメル麦" } },
+          { kind: "category", category: { name: "紙" } },
+          { kind: "category", category: { name: "鉱石" } },
+        ],
+      },
+    },
+    {
+      name: "ゼッテル",
+      category: [{ name: "紙" }, { name: "燃料" }],
+      synthesisLevel: 5,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "植物類" } },
+          { kind: "category", category: { name: "水" } },
+          { kind: "category", category: { name: "中和剤" } },
+        ],
+      },
+    },
   ];
 
   // Act
@@ -31,11 +78,23 @@ Deno.test("循環調合を探す", () => {
     synthesisItems,
     "薬のもと",
   );
+  const result2 = detectCircularSynthesisPathsBottomUp(
+    materialItems,
+    synthesisItems,
+    "中和剤・赤",
+  );
 
   // Assert
   assert(result.length === 1);
   assert(result[0][0].name === "薬のもと");
   assert(result[0][1].name === "薬のもと");
+
+  assert(result2.length === 1);
+  assert(result2[0].length === 4);
+  assert(result2[0][0].name === "中和剤・赤");
+  assert(result2[0][1].name === "ゼッテル");
+  assert(result2[0][2].name === "ラーメル麦粉");
+  assert(result2[0][3].name === "中和剤・赤");
 });
 
 Deno.test("素材で探してもエラーにならない", () => {
