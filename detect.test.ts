fef51c8d97b36702da57c48beefa29bd150cbe74
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 13:37:48 +0900

    RED: 探索する関数のインターフェースを作る

---
+++ detect.test.ts
@@ -0,0 +1,38 @@
+import { assert } from "jsr:@std/assert";
+
+import { detectCircularSynthesisPathsBottomUp } from "./detect.ts";
+import { MaterialItem, SynthesisItem } from "./items.ts";
+
+Deno.test("循環調合を探す", () => {
+  // Arrange
+  const materialItems: MaterialItem[] = [
+    {
+      name: "マジックグラス",
+      category: [{ name: "植物類" }, { name: "薬の材料" }, { name: "中和剤" }],
+    },
+  ];
+  const synthesisItems: SynthesisItem[] = [
+    {
+      name: "薬のもと",
+      category: [{ name: "薬の材料" }],
+      synthesisLevel: 15,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "薬の材料" } },
+          { kind: "category", category: { name: "植物類" } },
+        ],
+      },
+    },
+  ];
+
+  // Act
+  const result = detectCircularSynthesisPathsBottomUp(
+    materialItems,
+    synthesisItems,
+    "薬のもと",
+  );
+
+  // Assert
+  assert(result.length === 1);
+  assert(result[0][0].name === "薬のもと");
+});
---
+++ detect.ts
@@ -0,0 +1,9 @@
+import { ItemName, MaterialItem, SynthesisItem } from "./items.ts";
+
+export const detectCircularSynthesisPathsBottomUp = (
+  materiarItems: MaterialItem[],
+  synthesisItems: SynthesisItem[],
+  targetItemName: ItemName,
+): SynthesisItem[][] => {
+  return [];
+};
