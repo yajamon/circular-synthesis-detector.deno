2bded60aa587dbb9b46e379bbce5f9e559b3d53d
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 14:27:09 +0900

    テスト追加: 素材で探してもエラーにならない

--- detect.test.ts
+++ detect.test.ts
@@ -36,3 +36,36 @@ Deno.test("循環調合を探す", () => {
   assert(result.length === 1);
   assert(result[0][0].name === "薬のもと");
 });
+
+Deno.test("素材で探してもエラーにならない", () => {
+  // Arrange
+  const materialItems: MaterialItem[] = [
+    {
+      name: "マジックグラス",
+      category: [{ name: "植物類" }, { name: "薬の材料" }, { name: "中和剤" }],
+    },
+  ];
+  const synthesisItems: SynthesisItem[] = [
+    {
+      name: "薬のもと",
+      category: [{ name: "薬の材料" }],
+      synthesisLevel: 15,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "薬の材料" } },
+          { kind: "category", category: { name: "植物類" } },
+        ],
+      },
+    },
+  ];
+
+  // Act
+  const result = detectCircularSynthesisPathsBottomUp(
+    materialItems,
+    synthesisItems,
+    "マジックグラス",
+  );
+
+  // Assert
+  assert(result.length === 0);
+});
