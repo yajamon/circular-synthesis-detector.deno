117d3fc885a62a72b9fe62fdcbde8bc1d6d7cfcf
yajamon <yajamon.tatsuki@gmail.com>
2024-11-24 16:15:30 +0900

    RED: 指定アイテムからのレシピ逆引きが未実装なので穴をつく
    普通にそのまま実装してても良かったんじゃないの？ってのは感覚としてあるけども。

--- detect.test.ts
+++ detect.test.ts
@@ -129,3 +129,87 @@ Deno.test("素材で探してもエラーにならない", () => {
   // Assert
   assertEquals(result.length, 0);
 });
+
+Deno.test("素材が直接指定されているケース", () => {
+  // Arrange
+  const materialItems: MaterialItem[] = [
+    {
+      name: "マジックグラス",
+      category: [{ name: "植物類" }, { name: "薬の材料" }, { name: "中和剤" }],
+    },
+    {
+      name: "ラーメル麦",
+      category: [{ name: "食材" }, { name: "植物類" }],
+    },
+    {
+      name: "カーエン石",
+      category: [{ name: "鉱石" }, { name: "火薬" }],
+    },
+    {
+      name: "おいしい水",
+      category: [{ name: "水" }],
+    },
+  ];
+  const synthesisItems: SynthesisItem[] = [
+    {
+      name: "薬のもと",
+      category: [{ name: "薬の材料" }],
+      synthesisLevel: 15,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "薬の材料" } },
+          { kind: "category", category: { name: "植物類" } },
+        ],
+      },
+    },
+    {
+      name: "中和剤・赤",
+      category: [{ name: "中和剤" }],
+      synthesisLevel: 2,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "火薬" } },
+          { kind: "specific", item: { name: "おいしい水" } },
+        ],
+      },
+    },
+    {
+      name: "ラーメル麦粉",
+      category: [{ name: "食材" }, { name: "火薬" }],
+      synthesisLevel: 3,
+      recipe: {
+        items: [
+          { kind: "specific", item: { name: "ラーメル麦" } },
+          { kind: "specific", item: { name: "ゼッテル" } },
+          { kind: "category", category: { name: "鉱石" } },
+        ],
+      },
+    },
+    {
+      name: "ゼッテル",
+      category: [{ name: "紙" }, { name: "燃料" }],
+      synthesisLevel: 5,
+      recipe: {
+        items: [
+          { kind: "category", category: { name: "植物類" } },
+          { kind: "category", category: { name: "水" } },
+          { kind: "category", category: { name: "中和剤" } },
+        ],
+      },
+    },
+  ];
+
+  const result = detectCircularSynthesisPathsBottomUp(
+    materialItems,
+    synthesisItems,
+    "中和剤・赤",
+  );
+
+  // Assert
+  assertEquals(result.length, 1);
+  assertEquals(result[0].length, 4);
+  assertEquals(result[0][0].name, "中和剤・赤");
+  assertEquals(result[0][1].name, "ゼッテル");
+  assertEquals(result[0][2].name, "ラーメル麦粉");
+  assertEquals(result[0][3].name, "中和剤・赤");
+});
