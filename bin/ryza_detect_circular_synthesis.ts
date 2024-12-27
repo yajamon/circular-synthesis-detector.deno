b90f82188fa76fd60ef182836fc3412fda0d3beb
yajamon <yajamon.tatsuki@gmail.com>
2024-12-27 17:09:23 +0900

    引数パーサーを使ってみる

--- bin/ryza_detect_circular_synthesis.ts
+++ bin/ryza_detect_circular_synthesis.ts
@@ -2,12 +2,16 @@ import { detectCircularSynthesisPathsBottomUp } from "../detect.ts";
 
 import { materialItems, synthesisItems } from "../dataset/ryza.ts";
 
-if (Deno.args.length < 1) {
+import { parseArgs } from "jsr:@std/cli";
+
+const parsed = parseArgs(Deno.args, {});
+
+if (parsed._.length < 1) {
   console.error("Usage: deno run task_ryza.ts <item name>");
   Deno.exit(1);
 }
 
-const itemName = Deno.args[0];
+const itemName = parsed._[0].toString();
 
 const result = detectCircularSynthesisPathsBottomUp(
   materialItems,
